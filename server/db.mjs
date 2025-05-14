e99225afe9acb4d8dcbd5fcc60bebaa55302f85b
spiral987 <41581839+spiral987@users.noreply.github.com>
2025-05-14 11:08:31 +0900

    'cannnotloginomg'

--- server/db.mjs
+++ server/db.mjs
@@ -9,7 +9,8 @@ const dbPromise = open({
 
 export async function hashPassword(password){
   const saltRounds = 10;
-  const salt = await bcrypt.genSalt(password, saltRounds);
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
 }
 
