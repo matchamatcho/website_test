4430d23dbd5f61f92a6e4c66c7bbe6303fd94e57
Tako50 <kota22650@gmail.com>
2025-05-15 18:19:44 +0900

    ログイン

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,4 +1,3 @@
-// /src/firebase/firebase.ts
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
@@ -7,15 +6,17 @@ import {
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// デバッグ用: APIキーが正しく読み込まれているか確認
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 // Firebaseの設定
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Firebase初期化
